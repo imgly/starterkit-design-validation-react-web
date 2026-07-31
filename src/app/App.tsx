4fcd85e633ed499736ac84e0068cf603ab03f599
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-07-31 22:46:09 +0200

    Update starterkit for v1.80.0-rc.0

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -33,7 +33,7 @@ export function App({ editorConfig }: AppProps) {
     await initDesignValidationEditor(instance);
 
     // Load the scene
-    await instance.loadFromURL(resolveAssetPath('/assets/example.scene'));
+    await instance.load(resolveAssetPath('/assets/example.scene'));
 
     setCesdk(instance);
   }, []);
